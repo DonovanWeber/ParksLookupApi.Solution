d243044ec752cc12e3eb2d4df6662d645e1ee6b1
Donovan Weber <donovanweber03@gmail.com>
2022-08-19 15:07:40 -0400

    BUGFIX invalid expression for models

--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -7,26 +7,23 @@ namespace ParksLookup.Models
   {
     public ParksLookupContext(DbContextOptions<ParksLookupContext> options) : base(options)
     {
-
     }
 
-    public DbSet<Park> Parks { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
       builder.Entity<Park>()
           .HasData(
-              new Park { ParkId = 1, ParkName = "Yellowstone National Park", ParkDescription ="wilderness recreations area atop a volcanic hot spot" StateName = "Wyoming"},
-              new Park { ParkId = 2, ParkName = "Yosemite National Park", ParkDescription ="Inside California's Sierra Nevada mountains" StateName = "California"},
-              new Park { ParkId = 3, ParkName = "Grand Teton National Park", ParkDescription ="Popular summer destination for mountaineering, hiking, backcountry camping and fishing" StateName = "Wyoming"},
-              new Park { ParkId = 4, ParkName = "Grand Canyon National Park", ParkDescription ="" StateName = "Arizona"},
-              new Park { ParkId = 5, ParkName = "", ParkDescription ="" StateName = ""},
-              new Park { ParkId = 6, ParkName = "", ParkDescription ="" StateName = ""},
-              new Park { ParkId = 7, ParkName = "", ParkDescription ="" StateName = ""},
-              new Park { ParkId = 8, ParkName = "", ParkDescription ="" StateName = ""},
+              new Park { ParkId = 1, ParkName = "Yellowstone National Park", ParkDescription ="wilderness recreations area atop a volcanic hot spot", StateName = "Wyoming"},
+              new Park { ParkId = 2, ParkName = "Yosemite National Park", ParkDescription ="Inside California's Sierra Nevada mountains", StateName = "California"},
+              new Park { ParkId = 3, ParkName = "Grand Teton National Park", ParkDescription ="Popular summer destination for mountaineering, hiking, backcountry camping and fishing", StateName = "Wyoming"},
+              new Park { ParkId = 4, ParkName = "Grand Canyon National Park", ParkDescription ="Wide views of canyons revealing layered bands of red rock" ,StateName = "Arizona"},
+              new Park { ParkId = 5, ParkName = "Glacier National Park", ParkDescription ="Glacier carved peaks and valleys running to the Canadian border", StateName = "Montana"}
+       
 
-          )
+          );
 
     }
+    public DbSet<Park> Parks { get; set; }
   }
 }
