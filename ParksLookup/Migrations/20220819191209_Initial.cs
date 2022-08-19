086997668f5fea0579e7f1454304121cd225257d
Donovan Weber <donovanweber03@gmail.com>
2022-08-19 16:05:02 -0400

    WIP apis crud functionality through Postman

---
+++ ParksLookup/Migrations/20220819191209_Initial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ParkDescription = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    StateName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkDescription", "ParkName", "StateName" },
+                values: new object[,]
+                {
+                    { 1, "wilderness recreations area atop a volcanic hot spot", "Yellowstone National Park", "Wyoming" },
+                    { 2, "Inside California's Sierra Nevada mountains", "Yosemite National Park", "California" },
+                    { 3, "Popular summer destination for mountaineering, hiking, backcountry camping and fishing", "Grand Teton National Park", "Wyoming" },
+                    { 4, "Wide views of canyons revealing layered bands of red rock", "Grand Canyon National Park", "Arizona" },
+                    { 5, "Glacier carved peaks and valleys running to the Canadian border", "Glacier National Park", "Montana" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
---
+++ ParksLookup/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ParksLookup.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksLookup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ParksLookupContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parks Lookup API", Version = "v1", Description = "Parks Search Web API" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksLookupApi.Solution v1"));
+            }
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
